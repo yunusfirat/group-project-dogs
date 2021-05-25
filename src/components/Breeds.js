8f7c0ce0d4efde8084c5d9475abf6021def730bb
Mustafa <30720101+akbasmustafa@users.noreply.github.com>
2021-05-25 07:43:36 +0100

    Done Breed Select

--- src/components/Breeds.js
+++ src/components/Breeds.js
@@ -1,20 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Breeds.css";
 
 const Breeds = () => {
+  const options = [
+    "beagle",
+    "bluetick",
+    "bullterrier/staffordshire",
+    "malinois",
+    "wolfhound/irish"
+  ];
+
+  const [selectedPhoto, setSelectedPhoto] = useState(null);
+  const [selectState, setSelectState] = useState(options[0]);
+
+  useEffect(() => {
+    fetchRandomPhoto(selectState);
+  }, [selectState]);
+
+  function fetchRandomPhoto(breed) {
+    fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
+      .then((res) => res.json())
+      .then((data) => setSelectedPhoto(data.message))
+      .catch((err) => alert(err));
+  }
+
+  function handleSelect(e) {
+    setSelectState(e.target.value);
+  }
+
   return (
     <div className="Breeds">
       <h2 className="Breeds-title">Select a Breed</h2>
       <p>
-        <select className="Breeds-select">
-          <option>beagle</option>
-          <option>bluetick</option>
-          <option>bullterrier-staffordshire</option>
-          <option>malinois</option>
-          <option>wolfhound-irish</option>
+        <select className="Breeds-select" value={selectState} onChange={handleSelect}>
+          {options.map((breedName, index) => (
+            <option key={index} value={breedName}>
+              {breedName}
+            </option>
+          ))}
         </select>
       </p>
-      <img className="Breeds-image" src="http://via.placeholder.com/300x300" />
+      <img
+        className="Breeds-image"
+        src={selectedPhoto ? selectedPhoto : "http://via.placeholder.com/300x300"}
+        alt={selectedPhoto ? selectedPhoto.slice(30, 6) : "empty"}
+      />
       <p>
         <button className="Breeds-button">Show me more!</button>
       </p>
